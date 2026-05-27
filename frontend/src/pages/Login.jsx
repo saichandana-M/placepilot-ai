8e376aeea22e4cd14aca2d43fe6496f0122bc5ef
saichandana-M <maheshwaramsaichandana@gmail.com>
2026-05-27 17:20:27 +0530

    Added landing page and feature cards

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,64 @@
+function Login() {
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        background: "linear-gradient(135deg, #CFF7F0, #D8ECFF)",
+      }}
+    >
+      <div
+        style={{
+          backgroundColor: "white",
+          padding: "40px",
+          borderRadius: "18px",
+          width: "350px",
+          boxShadow: "0 8px 20px rgba(0,0,0,0.08)",
+        }}
+      >
+        <h2>Login</h2>
+
+        <input
+          type="email"
+          placeholder="Enter email"
+          style={{
+            width: "100%",
+            padding: "12px",
+            marginTop: "15px",
+            borderRadius: "10px",
+          }}
+        />
+
+        <input
+          type="password"
+          placeholder="Enter password"
+          style={{
+            width: "100%",
+            padding: "12px",
+            marginTop: "15px",
+            borderRadius: "10px",
+          }}
+        />
+
+        <button
+          style={{
+            width: "100%",
+            padding: "12px",
+            marginTop: "20px",
+            border: "none",
+            borderRadius: "10px",
+            backgroundColor: "#14B8A6",
+            color: "white",
+            cursor: "pointer",
+          }}
+        >
+          Sign In
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
